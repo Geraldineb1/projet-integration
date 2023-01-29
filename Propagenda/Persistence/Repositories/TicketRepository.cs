ab433c3a40ff2ee0fa6b70ed9b804cfab9841ff6
Geraldine Buxant <56636807+Geraldineb1@users.noreply.github.com>
2023-01-29 19:17:50 +0100

    Added Persistence/Repositories

---
+++ Propagenda/Persistence/Repositories/TicketRepository.cs
@@ -0,0 +1,20 @@
+﻿using Application.Persistence.Contracts;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+    public class TicketRepository : GenericRepository<Ticket>, ITicketRepository
+    {
+        private readonly PropagendaDbContext _dbContext;
+        public TicketRepository(PropagendaDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
---
+++ Propagenda/Persistence/Repositories/TicketReservationRepository.cs
@@ -0,0 +1,19 @@
+﻿using Application.Persistence.Contracts;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+    public class TicketReservationRepository : GenericRepository<TicketReservation>, ITicketReservationRepository
+    {
+        private readonly PropagendaDbContext _dbContext;
+        public TicketReservationRepository(PropagendaDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
