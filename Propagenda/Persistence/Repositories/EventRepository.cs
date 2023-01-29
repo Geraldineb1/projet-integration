342eee29472646a585373e035a27fd2a26a43659
Roxane Ancis <roxane.ancis@gmail.com>
2023-01-29 17:01:01 +0100

    add event

---
+++ Propagenda/Persistence/Repositories/EventRepository.cs
@@ -0,0 +1,19 @@
+﻿using Application.Persistence.Contracts;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+    public class EventRepository : GenericRepository<Event>, IEventRepository
+    {
+        private readonly PropagendaDbContext _dbContext;
+        public EventRepository(PropagendaDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
