7656d1024c83dde7e6f59483fea9837b05b43a7e
Roxane Ancis <roxane.ancis@gmail.com>
2023-01-29 12:32:10 +0100

    Update MappingProfile.cs

--- Propagenda/Application/Profiles/MappingProfile.cs
+++ Propagenda/Application/Profiles/MappingProfile.cs
@@ -3,6 +3,7 @@ using Application.DTOs.Event;
 using Application.DTOs.Provider;
 using Application.DTOs.Service;
 using Application.DTOs.ServiceReservation;
+using Application.DTOs.ServiceReservationService;
 using Application.DTOs.ServicerReservationService;
 using Application.DTOs.ServiceType;
 using Application.DTOs.Ticket;
@@ -23,13 +24,34 @@ namespace Application.Profiles
         {
             CreateMap<Service, ServiceDto>().ReverseMap();
             CreateMap<Service, ServiceListDto>().ReverseMap();
+            CreateMap<Service, ChangeServiceActivationDto>().ReverseMap();
+            CreateMap<Service, CreateServiceDto>().ReverseMap();
+            CreateMap<Service, UpdateServiceDto>().ReverseMap();
+
             CreateMap<Provider, ProviderDto>().ReverseMap();
+            CreateMap<Provider, CreateProviderDto>().ReverseMap();
+            CreateMap<Provider, UpdateProviderDto>().ReverseMap();
+            CreateMap<Provider, ChangeProviderActivationDto>().ReverseMap();
+
             CreateMap<Event, EventDto>().ReverseMap();
+            CreateMap<Event, CreateEventDto>().ReverseMap();
+
             CreateMap<ServiceReservation, ServiceReservationDto>().ReverseMap();
+            CreateMap<ServiceReservation, CreateServiceReservationDto>().ReverseMap();
+            CreateMap<ServiceReservation, ChangeServiceReservationApprovalDto>().ReverseMap();
+            CreateMap<ServiceReservation, ChangeServiceReservationCompletionDto>().ReverseMap();
+
             CreateMap<ServiceReservationService, ServiceReservationServiceDto>().ReverseMap();
+            CreateMap<ServiceReservationService, CreateServiceReservationServiceDto>().ReverseMap();
+
             CreateMap<ServiceType, ServiceTypeDto>().ReverseMap();
+            CreateMap<ServiceType, CreateServiceTypeDto>().ReverseMap();
+
             CreateMap<Ticket, TicketDto>().ReverseMap();
+            CreateMap<Ticket, CreateTicketDto>().ReverseMap();
+
             CreateMap<TicketReservation, TicketReservationDto>().ReverseMap();
+            CreateMap<TicketReservation, CreateTicketReservationDto>().ReverseMap();
         }
     }
 }
