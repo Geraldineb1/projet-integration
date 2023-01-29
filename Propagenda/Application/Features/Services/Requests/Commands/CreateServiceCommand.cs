139c92491b029773e6dc77abab041a934dfac326
Roxane Ancis <roxane.ancis@gmail.com>
2023-01-29 11:25:15 +0100

    response

--- Propagenda/Application/Features/Services/Requests/Commands/CreateServiceCommand.cs
+++ Propagenda/Application/Features/Services/Requests/Commands/CreateServiceCommand.cs
@@ -1,4 +1,5 @@
 ﻿using Application.DTOs.Service;
+using Application.Responses;
 using MediatR;
 using System;
 using System.Collections.Generic;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace Application.Features.Services.Requests.Commands
 {
-    public class CreateServiceCommand : IRequest<int>
+    public class CreateServiceCommand : IRequest<BaseCommandResponse>
     {
         public CreateServiceDto serviceDto { get; set; }
     }
