0d3d4964198fcc0d13dfff3fb2fe00c081ebcf11
Geraldine Buxant <56636807+Geraldineb1@users.noreply.github.com>
2023-03-31 17:01:48 +0200

    TicketReservation

---
+++ Propagenda/PropagendaMVC/Controllers/TicketReservationsController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PropagendaMVC.Contracts;
+using PropagendaMVC.Models;
+
+namespace PropagendaMVC.Controllers
+{
+    public class TicketReservationsController : Controller
+    {
+        private readonly ITicketReservationService _ticketReservationService;
+        public TicketReservationsController(ITicketReservationService ticketReservationService)
+        {
+            this._ticketReservationService = ticketReservationService;
+        }
+
+        // GET: TicketReservationController
+        public async Task<ActionResult> Index()
+        {
+            var model = await _ticketReservationService.GetTicketReservations();
+            return View(model);
+        }
+
+        // GET: TicketReservationController/Details/5
+        public async Task<ActionResult> Details(int id)
+        {
+            var model = await _ticketReservationService.GetTicketReservationDetails(id);
+            return View(model);
+        }
+
+        // GET: TicketReservationController/Create
+        public async Task<ActionResult> Create()
+        {
+            return View();
+        }
+
+        // POST: TicketReservationController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(CreateTicketReservationVM ticketReservation)
+        {
+            try
+            {
+                var response = await _ticketReservationService.CreateTicketReservation(ticketReservation);
+                if (response.Success)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                ModelState.AddModelError("", response.ValidationErrors);
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+            }
+
+            return View(ticketReservation);
+        }
+
+    }
+}
---
+++ Propagenda/PropagendaMVC/Models/TicketReservationVM.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace PropagendaMVC.Models
+{
+        public class TicketReservationVM : CreateTicketReservationVM
+        {
+            public int Id { get; set; }
+        }
+
+        public class CreateTicketReservationVM
+        {
+            [Required]
+            public int TotalNbTickets { get; set; }
+
+
+            [Required]
+            public int EventId { get; set; }
+
+        }
+}
